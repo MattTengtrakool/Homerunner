6ac5d2e49af12c7f06d258f3b0de91b6cf31d3eb
caraxyu <90162375+caraxyu@users.noreply.github.com>
2021-12-05 22:07:39 -0500

    Add files via upload

--- SQL.sql
+++ SQL.sql
@@ -1,26 +1,29 @@
 
-CREATE DATABASE HomeRunnder;
-Use HomeRunnder;
-CREATE TABLE [dbo].[Person](
-	[PersonId]  [int] IDENTITY(1,1) NOT NULL,
-	[CorrespSalutationFormat] [varchar](1) NULL,
-	[FirstName] [varchar](30) NOT NULL,
-	[LastName] [varchar](30) NOT NULL,	
-	[PersonType][varchar](30) NOT NULL,
-	[Address] [varchar](255) NULL,
-	City [varchar](50) NULL,
-	State [varchar](50) NULL,
-	Country [varchar](50) NULL,
-	ZipCode [varchar](10) NULL,	
-	[Cellphone] [varchar](20) NULL,
-	[BusinessPhone] [varchar](20) NULL,
-	[Email] [varchar](100)  NULL,
-	[UserName]  [varchar](100) NOT NULL,
-	[Password] [varchar](100) NOT NULL,
-[notes] [varchar](255) NULL
- CONSTRAINT [pk_person] PRIMARY KEY CLUSTERED 
-(
-	[PersonId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+sqlite3 homerunner.db
+
+CREATE TABLE IF NOT EXISTS users(
+	id  INTEGER PRIMARY KEY AUTOINCREMENT,  
+	UserName  text  UNIQUE NOT NULL,
+	hash text NOT NULL,
+	UserType text  NOT NULL,
+	FirstName text NOT NULL,
+	LastName text NOT NULL,	
+	Address  text NULL,
+	City text NULL,
+	State text NULL,
+	Country text NULL,
+	ZipCode text NULL,	
+	Phone text  NULL,	
+	Email text NOT NULL,	
+	Note text Null
+	);
+
+
+CREATE TABLE IF NOT EXISTS  contact_message(
+	id  INTEGER PRIMARY KEY AUTOINCREMENT,  
+	Name  text  UNIQUE NOT NULL,		
+	Email text NOT NULL,	
+	Phone text  NULL,	
+	Title text  NULL,	
+	Message text Null
+	);
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import os
 
 from cs50 import SQL
@@ -7,14 +8,16 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
-
 from helpers import apology, login_required, lookup, usd
 
-# Configure application
-app = Flask("__name__")
 
+# Configure application
+app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -22,7 +25,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///homerunner.db")    
+db = SQL("sqlite:///homerunner.db") 
+
 
 @app.after_request
 def after_request(response):
@@ -32,101 +36,161 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/")
-@login_required
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("register.html")
+    return render_template("index.html")
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        # get firstname, lastname, email, phone, username, password, and confirmation
-        firstname = request.form.get("fname")
-        lastname = request.form.get("lname")
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/about")
+def about():
+           return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    """Contact"""
+    if request.method == "POST":
+
+  # Ensure username was submitted
+        if not request.form.get("name"):
+            return apology("missing name")
+    
+        name = request.form.get("name")        
+        if not request.form.get("email"):
+            return apology("missing email") 
+
         email = request.form.get("email")
         phone = request.form.get("phone")
-        username = request.form.get("username")
-        password = request.form.get("password")
-        confirmation = request.form.get("confirmation")
-
-        # Ensure password was submitted
-        if not firstname:
-            return apology("must provide firstname", 400)
-            
-        # Ensure password was submitted
-        elif not lastname:
-            return apology("must provide lastname", 400)
-        # Ensure password was submitted
-        elif not email:
-            return apology("must provide email", 400)
-
-        # Ensure password was submitted
-        elif not phone:
-            return apology("must provide phone", 400)
-            
-        # Ensure username was submitted
-        elif not username:
-            return apology("must provide username", 400)
-
-        elif db.execute("SELECT * FROM users WHERE username = ?", username):
-            return apology("username in use, try another", 400)
-
-        # Ensure password was submitted
-        elif not password:
-            return apology("must provide password", 400)
-        
-        # Ensure password confirmation was submitted
-        elif not confirmation:
-            return apology("must provide password confirmation", 400)
-
-        # make sure tha pass/confirmation match
-        elif confirmation != password:
-            return apology("passwords do not match", 400)
-        
-        # get hash value and insert with user information
-        hash = generate_password_hash(password)
-        
-        nuser = db.execute("INSERT INTO users (username, hash) VALUES (?,?)", username, hash)
-        session["user_id"] = nuser
-
-        # Redirect user to home page
+        title = request.form.get("title")
+        message = request.form.get("message")
+        db.execute("INSERT INTO contact_message (name, email,  phone, title, message) VALUES (?, ?,?,?,?)", name, email,  phone, title, message)    
         return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("register.html")
+     return render_template("contact.html")               
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any userName
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    """Register user"""
+
+    # Clear session
+    session.clear()
+
     if request.method == "POST":
-        # get firstname, lastname, email, phone, username, password, and confirmation
-        firstname = request.form.get("fname")
-        lastname = request.form.get("lname")
+
+        # Make sure username provided
+        if not request.form.get("username"):
+            return apology("missing username")
+
+        # Get username
+        username = request.form.get("username")
+
+        # Make sure user doesn't exist
+        if len(db.execute("SELECT * FROM users WHERE username = ?", username)) != 0:
+            return apology("user already exists")
+
+        # Make sure password & confirmation provided
+        if not request.form.get("password") or not request.form.get("confirm_password"):
+            return apology("missing password")
+
+        # Get password & confirmation
+        password = request.form.get("password")
+        confirmation = request.form.get("confirm_password")
+
+        # Make sure password equals confirmation
+        if password != confirmation:
+            return apology("password and confirmation don't match")
+
+        # Hash password
+        password_hash = generate_password_hash(password)
+
+        firstname = request.form.get("firstname")
+        lastName = request.form.get("lastName")
+        usertype = request.form.get("usertype")
+        address = request.form.get("address")
+        city = request.form.get("city")
+        state = request.form.get("state")
+        country = request.form.get("country")
+        zipcode = request.form.get("zipcode")
+        phone = request.form.get("phone")
         email = request.form.get("email")
-        food = request.form.get("phone")
+        note = request.form.get("note")
 
-        # Ensure password was submitted
-        if not firstname:
-            return apology("must provide firstname", 400)
-            
-        # Ensure password was submitted
-        elif not lastname:
-            return apology("must provide lastname", 400)
 
-        # Ensure password was submitted
-        elif not email:
-            return apology("must provide email", 400)
+        # Add user to users table
+        
+        try:
+            id =db.execute("INSERT INTO users (username, hash,usertype, firstname,lastName, address, city, state, country, zipcode, phone, email, note) VALUES (?, ?,?,?,?,?,?, ?,?,?,?,?,?)", username, password_hash, usertype, firstname,lastName,address, city, state, country, zipcode, phone, email, note)
+         
+        except ValueError:
+            return apology("username taken")
 
-        # Ensure password was submitted
-        elif not food:
-            return apology("must provide food", 400)
-            
-        nuser = db.execute("INSERT INTO users (username, hash) VALUES (?,?)", username, hash)
-        session["user_id"] = nuser
+          # Log user in
+        session["user_id"] = id
 
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
+        # Let user know they're registered
+        flash("Registered!")
+        return redirect("/")   
+       
     else:
+
         return render_template("register.html")
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
+
+
